44f28e1752ea5f4f858566417d5171f85bfbc107
KacperZajac12 <244743@student.uek.krakow.pl>
2025-11-15 18:00:00 +0100

    Initial commit

--- 7.6.py
+++ 7.6.py
@@ -1,7 +1,15 @@
 number = input("Input credit card number: ")
 
-for char in number:
-    count +=1
-    if count > 2 and count < 12:
-        char.replace(char,"*")
+def invisible_number(number):
+    count = 0
+    new_number = "  "
+    for char in number:
+        count +=1
+        if count > 2 and count < len(number)-3:
+            new_number += "*"
+        else:
+            new_number += char
+    return new_number
+
+print(invisible_number(number))
     
