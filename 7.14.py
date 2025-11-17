93d60e663d16a9eda0958daf14f21522532750c1
KacperZa <lacper.zajac765@gmail.com>
2025-11-17 21:38:42 +0100

    Minimal changes

--- 7.14.py
+++ 7.14.py
@@ -1,4 +1,6 @@
 def solving(number1, number2, operator):
+    number1 = int(number1)
+    number2 = int(number2)
     if operator == "+":
         sum = number1 + number2
     elif operator == "%":
@@ -11,5 +13,6 @@ def solving(number1, number2, operator):
         sum = number1- number2
     return sum
 
-user_input = input("Podaj dwie liczby i operator, wszystko oddzielone spacją")
+user_input = input("Podaj dwie liczby i operator, wszystko oddzielone spacją: ")
 user_number1, user_number2, user_operator = user_input.split()
+print(solving(user_number1,user_number2,user_operator))
